5cfec8716cc7ee4cf704e15b3fff497bd20203b7
Gaeulbyul <aeiou256@gmail.com>
2016-07-26 23:44:59 +0900

    Improve loading configure file

--- app/config.js
+++ app/config.js
@@ -1,18 +1,23 @@
 const path = require('path');
 const fs = require('fs');
 
+const defaultConfigPath = path.join(__dirname, '../app/default/def_conf.json');
+
 const Config = {
+  _defaultConfig: JSON.parse(fs.readFileSync(defaultConfigPath)),
   _filePath: path.join(__dirname, '../config.json'),
   load () {
-    console.info('filePath: "%s"', this._filePath);
-    var json = fs.readFileSync(this._filePath);
-    json = JSON.parse(json);
-    return json;
+    var config = this._defaultConfig;
+    var userConfig = JSON.parse(fs.readFileSync(this._filePath));
+    Object.assign(config, userConfig);
+    return config;
   },
   save (newConfig) {
     const jsonStr = JSON.stringify(newConfig, null, 2);
     fs.writeFileSync(this._filePath, jsonStr);
   },
 };
+
+
 
 module.exports = Config;
