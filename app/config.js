efc4e639ee14f91b836a282ec6227b2ed12385e3
Dalgona <dalgona@hontou.moe>
2016-08-06 19:51:01 +0900

    Fix error when starting without config.json

--- app/config.js
+++ app/config.js
@@ -5,9 +5,17 @@ const Config = {
   _filePath: path.join(__dirname, '../config.json'),
   load () {
     console.info('filePath: "%s"', this._filePath);
-    var json = fs.readFileSync(this._filePath);
-    json = JSON.parse(json);
-    return json;
+    try {
+      let json = fs.readFileSync(this._filePath);
+      json = JSON.parse(json);
+      return json;
+    }
+    catch (e) {
+      console.warn("Cannot read `config.json`. Reading default configuration.");
+      let json = fs.readFileSync(path.join(__dirname, 'default/def_conf.json'));
+      json = JSON.parse(json);
+      return json;
+    }
   },
   save (newConfig) {
     const jsonStr = JSON.stringify(newConfig, null, 2);
